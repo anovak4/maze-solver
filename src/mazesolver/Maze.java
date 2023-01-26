1ff341df02f30f5ee1b2ca0b560c77e1b86e1678
Anna Novak <annanovak357@gmail.com>
2023-01-26 00:45:34 -0500

    formatting and printing updates

--- src/mazesolver/Maze.java
+++ src/mazesolver/Maze.java
@@ -1,75 +1,85 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mazesolver;
 
-/**
- *
- * @author robno
- * 
- * from computer's book pages 462-463
- * with modifications
- */
+
 public class Maze {
-    private final int TRIED = 3;
-    private final int PATH = 7;
-    private int[][] grid;
-    
+    private final char TRIED = '~';
+    private final char PATH = '*';
+    private char[][] grid;
+
     public Maze(int rows, int columns) {
-        grid = new int[rows][columns];
-        for(int i = 0; i < grid.length; i++)
-            for(int j = 0; j < grid[i].length; j++)
-                grid[i][j] = (int)(Math.random() * 2);
-        grid[0][0] = 1;
-        grid[grid.length - 1][grid[0].length - 1] = 1;
+        grid = new char[rows][columns];
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[i].length; j++) {
+                int rand = (int) (Math.random() * 2);
+                if (rand == 0)
+                    grid[i][j] = '#';
+                else
+                    grid[i][j] = ' ';
+            }
+        grid[0][0] = ' ';
+        grid[grid.length - 1][grid[0].length - 1] = ' ';
     }
-    
+
     public boolean traverse(int row, int column) {
         boolean done = false;
-        if(valid(row, column)) {
+        if (valid(row, column)) {
             grid[row][column] = TRIED;
-            if(row == grid.length - 1 && column == grid[0].length - 1)
+            if (row == grid.length - 1 && column == grid[0].length - 1)
                 done = true;
             else {
                 done = traverse(row + 1, column);
-                if(!done)
+                if (!done)
                     done = traverse(row, column + 1);
-                if(!done)
+                if (!done)
                     done = traverse(row - 1, column);
-                if(!done)
+                if (!done)
                     done = traverse(row, column - 1);
-                if(!done)
+                if (!done)
                     done = traverse(row - 1, column - 1);
-                if(!done)
+                if (!done)
                     done = traverse(row - 1, column + 1);
-                if(!done)
+                if (!done)
                     done = traverse(row + 1, column - 1);
-                if(!done)
+                if (!done)
                     done = traverse(row + 1, column + 1);
             }
-            if(done)
+            if (done)
                 grid[row][column] = PATH;
         }
         return done;
     }
-    
+
     private boolean valid(int row, int column) {
-        if(row >= 0 && row < grid.length && column >= 0 && column < grid[row].length)
-            if(grid[row][column] == 1)
+        if (row >= 0 && row < grid.length && column >= 0 && column < grid[row].length)
+            if (grid[row][column] == ' ')
                 return true;
         return false;
     }
-    
+
     @Override
     public String toString() {
-        String result = "\n";
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[i].length; j++)
-                result += grid[i][j] + " ";
-            result += "\n";
+        String result = "\n--";
+        for (int i = 0; i < grid[0].length; i++) {
+            result += "--";
         }
+        result += "-\n";
+        for (int i = 0; i < grid.length; i++) {
+            result += "| ";
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '*') {
+                    result += "\u001B[32m" + grid[i][j] + "\u001B[0m" + " "; // print * in green
+                } else if (grid[i][j] == '~') {
+                    result += "\u001B[31m" + grid[i][j] + "\u001B[0m" + " "; // print ~ in red
+                } else {
+                    result += grid[i][j] + " ";
+                }
+            }
+            result += "| \n";
+        }
+        for (int i = 0; i < grid[0].length; i++) {
+            result += "--";
+        }
+        result += "---";
         return result;
     }
 }
--- src/mazesolver/MazeSolver.java
+++ src/mazesolver/MazeSolver.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mazesolver;
 
-/**
- *
- * @author robno
- */
+
 public class MazeSolver {
 
     /**
@@ -19,9 +11,9 @@ public class MazeSolver {
         System.out.println("0 = a blocked path\n1 = a clear path\n3 = a tried path\n7 = a succesful path\nCan solve diagonally\n");
         System.out.println("The maze:\n" + maze);
         if(maze.traverse(0,0))
-            System.out.println("THE MAZE WAS SOLVED!!");
+            System.out.println("\nThe maze was solved!");
         else
-            System.out.println("There isn't a path.");
+            System.out.println("\nThere isn't a path.");
         System.out.println(maze);
         
     }
